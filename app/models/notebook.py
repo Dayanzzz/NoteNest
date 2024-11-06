3674da355366ed89c415e129e43346794f04d5e4
AbramBuck <abram.buck@gmail.com>
2024-11-06 17:07:19 -0600

    notebook model created not working

--- app/models/notebook.py
+++ app/models/notebook.py
@@ -1,17 +1,14 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
-from datetime import 
+from .db import db
+from datetime import datetime
 from flask_login import UserMixin
 
 class Notebook(db.Model, UserMixin):
-    __tablename__= 'notebooks'
+    __tablename__ = 'notebooks'
 
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
-
-        id = db.Column(db.Integer, primary_key=True)
-        owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-        name = db.Column(db.String(50))
-        created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-        updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
   
-        user = db.relationship('User', back_populates='notebooks')
+    user = db.relationship('User', back_populates='notebooks')
