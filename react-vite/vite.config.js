7280a031e6dcee6da37857e4faafaf1153b17520
AbramBuck <abram.buck@gmail.com>
2024-11-11 15:21:36 -0600

    vite config updated

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -3,16 +3,14 @@ import eslintPlugin from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig((mode) => ({
   plugins: [
     react(),
-    // Only add eslintPlugin if in development mode
-    mode === "development" &&
-      eslintPlugin({
-        lintOnStart: true,
-        failOnError: false, // You can also omit this as it’s already the default in development
-      }),
-  ].filter(Boolean), // Filter out false values to avoid errors
+    eslintPlugin({
+      lintOnStart: true,
+      failOnError: mode === "production",
+    }),
+  ],
   server: {
     open: true,
     proxy: {
@@ -20,25 +18,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-
-
-// import { defineConfig } from "vite";
-// import eslintPlugin from "vite-plugin-eslint";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig((mode) => ({
-//   plugins: [
-//     react(),
-//     eslintPlugin({
-//       lintOnStart: true,
-//       failOnError: mode === "production",
-//     }),
-//   ],
-//   server: {
-//     open: true,
-//     proxy: {
-//       "/api": "http://127.0.0.1:8000",
-//     },
-//   },
-// }));
