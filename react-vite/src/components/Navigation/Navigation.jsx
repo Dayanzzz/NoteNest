9cde7abd56df4c374d125b90cf8a41595253b3dc
AbramBuck <abram.buck@gmail.com>
2024-11-13 10:38:13 -0600

    Navigation Home button removed

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 function Navigation() {
+  const user = useSelector((store)=> store.session.user);
+
   return (
     <div className="wrapper">
       <div className="nav-link-area">
-        <div><NavLink to="/">Home</NavLink></div>
         <div><ProfileButton /></div>
       </div>
 
